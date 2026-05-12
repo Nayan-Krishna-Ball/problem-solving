db26d8b29723c855ffbd57498118c920ad8049f9
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-05-12 19:49:25 +0600

    Add curry function implementation for function argument handling

---
+++ Medium/Currying-in-js/currying.js
@@ -0,0 +1,14 @@
+export default function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      // Once we have enough arguments, execute the original function with the same receiver.
+      return func.apply(this, args);
+    }
+
+    return (arg) =>
+      arg === undefined
+        ? // Empty calls keep the chain alive without changing the collected arguments.
+          curried.apply(this, args)
+        : curried.apply(this, [...args, arg]);
+  };
+}
