a4788dd8a797b3d184738123f5ec219847f58949
Nayan Krishna <nayan.krishna710@gmail.com>
2026-05-13 00:05:52 +0600

    Add flaten array

---
+++ Medium/Flattaen-array/array.js
@@ -0,0 +1,12 @@
+export default function flatten(value) {
+ for (let i = 0; i < value.length; ) {
+    if (Array.isArray(value[i])) {
+     
+      value.splice(i, 1, ...value[i]);
+    } else {
+      i++;
+    }
+  }
+
+  return value;
+}
