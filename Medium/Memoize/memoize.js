17262fef495fb8302bce9840cec8c24a80140768
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-05-16 09:17:50 +0600

    Add memoize function to optimize function calls with caching

---
+++ Medium/Memoize/memoize.js
@@ -0,0 +1,16 @@
+//
+
+export default function memoize(func) {
+  let cache = {};
+
+  return function (...args) {
+    const key = JSON.stringify(args);
+    if (cache[key]) {
+      console.log("Returning cached result...");
+      return cache[key];
+    }
+    const result = func.apply(this, args);
+    cache[key] = result;
+    return result;
+  };
+}
