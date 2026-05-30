32e2e14381bb54932606a9c3fc09fcd3c0e33a88
Nayan Krishna <nayan.krishna710@gmail.com>
2026-05-31 00:04:44 +0600

    Update app.js

--- Easy/app.js
+++ Easy/app.js
@@ -1,1 +1,11 @@
-//hello All
+//find longest word 
+
+function longestWord(str) {
+  return str
+    .split(" ")
+    .reduce((longest, current) =>
+      current.length > longest.length ? current : longest
+    );
+}
+
+console.log(longestWord("I love javascript programming"));
