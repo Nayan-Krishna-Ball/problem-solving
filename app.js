9bf69c2ab48607739655122b90bc0a7d2fbb0609
Nayan Krishna <nayan.krishna710@gmail.com>
2026-06-09 09:12:45 +0600

    Update app.js

--- app.js
+++ app.js
@@ -22,3 +22,30 @@ for (let key in product) {
     }
   }
 }
+
+
+//Custom map function
+
+
+Array.prototype.myMap = function (callback) {
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+
+  return result;
+};
+
+const numbers = [1, 2, 3];
+
+console.log(
+  numbers.myMap(num => num * 2)
+);
+
+// [2,4,6]
+
+
+
+
+
