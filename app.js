3b75b5dd13ed6a4361f86528b660c7c97fbf4710
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-06-21 00:26:06 +0600

    Refactor app.js: Clean up whitespace and improve console.log formatting

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ const product = {
 for (let key in product) {
   //   console.log(`${key}: ${product[key]}`);
 
-
   if (typeof product[key] === "object") {
     for (let subkey in product[key]) {
       console.log(`${subkey}: ${product[key][subkey]}`);
@@ -23,9 +22,7 @@ for (let key in product) {
   }
 }
 
-
 //Custom map function
-
 
 Array.prototype.myMap = function (callback) {
   const result = [];
@@ -39,13 +36,25 @@ Array.prototype.myMap = function (callback) {
 
 const numbers = [1, 2, 3];
 
-console.log(
-  numbers.myMap(num => num * 2)
-);
+console.log(numbers.myMap((num) => num * 2));
 
 // [2,4,6]
 
+//FizzBuzz problem
 
+function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
 
-
-
+fizzBuzz(15);
+// 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz
