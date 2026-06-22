897d189f4a84ec9d2b4fb4b1838f82a2eb596fe3
Nayan Krishna <nayan.krishna710@gmail.com>
2026-06-22 22:27:15 +0600

    Update app.js

--- Easy/app.js
+++ Easy/app.js
@@ -9,3 +9,20 @@ function longestWord(str) {
 }
 
 console.log(longestWord("I love javascript programming"));
+
+//promise. all
+const p1 = Promise.resolve(1);
+
+const p2 = Promise.resolve(2);
+
+const p3 = Promise.resolve(3);
+
+myPromiseAll([p1, p2, p3])
+
+  .then(console.log)
+
+  .catch(console.error);
+
+// Output:
+
+// [1, 2, 3]
