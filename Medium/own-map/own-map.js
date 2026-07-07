ede8419a1f9665f9fd2e6234122099b88d47be77
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-07-08 00:59:19 +0600

    Add custom myMap implementation to Array prototype

---
+++ Medium/own-map/own-map.js
@@ -0,0 +1,19 @@
+//
+
+Array.prototype.myMap = function (callback) {
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    result.push(callback(this[i], i, this));
+  }
+
+  return result;
+};
+
+const numbers = [1, 2, 3, 4];
+
+const doubled = numbers.myMap((num) => num * 2);
+
+console.log(doubled);
+
+// Output: [2, 4, 6, 8]
