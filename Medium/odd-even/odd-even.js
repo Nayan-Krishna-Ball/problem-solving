b4fdccce1cf67c71ca2b7aa86d70283fde0f08e0
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-07-14 20:22:16 +0600

    Add checkEvenOdd function to determine if a number is even or odd

---
+++ Medium/odd-even/odd-even.js
@@ -0,0 +1,12 @@
+//
+
+function checkEvenOdd(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+
+console.log(checkEvenOdd(10)); // Even
+console.log(checkEvenOdd(7)); // Odd
