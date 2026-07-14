e44748bdfbcc9e585482454cc207a00e1ae5b6e8
Nayan-Krishna-Ball <nayan.krishna710@gmail.com>
2026-07-15 00:39:41 +0600

    add second largest solution

---
+++ Medium/second-largest-number/second-largest-number.js
@@ -0,0 +1,12 @@
+//
+
+function secondLargest(arr) {
+  let unique = [...new Set(arr)];
+
+  unique.sort((a, b) => b - a);
+
+  return unique[1];
+}
+
+console.log(secondLargest([10, 5, 30, 20, 30]));
+//20
